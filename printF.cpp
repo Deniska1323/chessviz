6ab2c83fe458834725bc75a6df64bf4024eff5fb
Denchik <89137714168@mail.ru>
2020-04-22 19:25:35 +0200

    print chesstable

---
+++ printF.cpp
@@ -0,0 +1,16 @@
+#include "printF.hpp"
+#include <iostream>
+using namespace std;
+
+void printF(char** aa)
+{
+    system("clear");
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            cout.width(4);
+            cout << aa[i][j];
+        }
+        cout << "\n";
+    }
+}
